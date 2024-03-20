52beae45f689ffa21de53888872120321b62cb0a
Kiaralei <112238682+Kiaralei@users.noreply.github.com>
2024-03-20 01:32:56 -0700

    Initial Commit

---
+++ Yelp-Business-Search-IOS/Yelp_Business_Search_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yelp_Business_Search_IOSApp.swift
+//  Yelp-Business-Search-IOS
+//
+//  Created by Kiara Lei on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Yelp_Business_Search_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
